88390630d5f27fc364060f29c0db0c0ebb783328
Sharafath Ahammed V <142240872+sharafath07@users.noreply.github.com>
2025-09-21 14:19:40 +0000

    finished

--- auction/src/components/ui/Button.jsx
+++ auction/src/components/ui/Button.jsx
@@ -25,7 +25,7 @@ export const Button = ({
   const sizes = {
     sm: 'px-3 py-1.5 text-sm',
     md: 'px-4 py-2 text-sm',
-    lg: 'px-6 py-3 text-base',
+    lg: 'px-6 text-base py-3 ',
   };
 
   const disabledClasses = 'opacity-50 cursor-not-allowed';
